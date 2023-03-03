4de0a00f93322b2673c15627ea54b34635ab565b
chema <chemamengibar@gmail.com>
2023-03-03 20:37:23 +0100

    add: viewer

--- client/src/views/Player.vue
+++ client/src/views/Player.vue
@@ -7,7 +7,7 @@ import IconShoot from "../components/action-icons/icon-shoot.vue";
 import IconStick from "../components/action-icons/icon-stick-handling.vue";
 import IconStop from "../components/action-icons/icon-stop-control.vue";
 import IconBack from "../components/action-icons/icon-back.vue";
-import { isProxy, toRaw } from 'vue';
+import { isProxy, toRaw } from "vue";
 
 export default {
   name: "Player",
@@ -15,81 +15,125 @@ export default {
   data: () => ({
     t: {},
     s: undefined,
-    cursor:0,
-    timer:0,
+    cursor: 0,
+    timer: 0,
     intervalid: 0,
     intervalInit: 0,
-    countdown:5,
-    countdownDefault:5
+    countdown: 5,
+    countdownDefault: 5,
+    flow: 'ready',
+    useCountdown: false
   }),
   methods: {
-    back:function(){
-     
-      //this.$router.push('/')
-
+    player: function(){
+      if(this.flow === 'ready'){
+        this.init();
+      }
+      else if(this.flow === 'running'){
+        this.pause();
+      }
+      else if(this.flow === 'paused'){
+        this.play();
+      }
+    },
+    fullScreen: function () {
       if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
       } else if (document.exitFullscreen) {
         document.exitFullscreen();
       }
     },
-    init:function(){
+    back: function () {
+      this.$router.push("/");
+    },
+    viewer: function () {
+      this.$router.push({ path: '/viewer', query: { id: this.s.id } })
+    },
+    init: function () {
+      this.cursor = 0;
       this.countdown = this.countdownDefault;
-      clearInterval(this.intervalInit)
-      this.intervalInit = setInterval(() => {
-        this.countdown--;
-        if(this.countdown <= 0){
-          clearInterval(this.intervalInit)
-          this.play()
-        }
-      }, 1000);
+      clearInterval(this.intervalInit);
+      if(this.useCountdown){
+        this.intervalInit = setInterval(() => {
+            this.countdown--;
+            if (this.countdown <= 0) {
+              clearInterval(this.intervalInit);
+              this.flow = "running"
+              this.play();
+            }
+          }, 1000);
+      }else{
+        this.flow = "running"
+        this.play();
+      }
     },
-    next: function(){
+    next: function () {
       this.cursor++;
-      this.play()
+      this.play();
     },
-    play: function(){
-      clearInterval(this.intervalid)
-      const currentFrame = toRaw(this.currentFrame)
-      if(currentFrame){
+    play: function () {
+      this.flow = 'running'
+      clearInterval(this.intervalid);
+      const currentFrame = toRaw(this.currentFrame);
+      if (currentFrame) {
         this.intervalid = setInterval(() => {
           this.next();
         }, currentFrame.duration * 1000);
       }
-    }
+    },
+    staticNext: function () {
+      clearInterval(this.intervalid);
+      this.flow = 'paused'
+      this.cursor++;
+      console.log('NEXT', toRaw(this.currentFrame))
+    },
+    staticPrev: function () {
+      clearInterval(this.intervalid);
+       this.flow = 'paused'
+      if(this.cursor < 1  ){
+        return
+      }
+      this.cursor--;
+    },
+    pause: function () {
+      this.flow = 'paused'
+      clearInterval(this.intervalid);
+     }
   },
   created() {
     this.t = this.$services.localeService.D();
   },
   mounted() {
-    const _ = this;
     const seqId = this.$route.query.id;
-    console.log('>>>', seqId) 
+    console.log(">>>", seqId);
     this.s = this.$services.toolService.getSequenceById(seqId);
-    console.log('>>>', this.s)  
+    console.log(">>>", this.s);
     this.cursor = 0;
-    // // Auto init
-    //this.play()
+    //this.fullScreen()
   },
   computed: {
     currentFrame() {
-      if(this.s){
-        if(this.cursor < this.s.frames.length ){
-          if(Array.isArray( this.s.frames[ this.cursor ])){
-            const max = Math.floor(this.s.frames[ this.cursor ].length - 1);
+      if (this.s) {
+        if (this.cursor < this.s.frames.length) {
+          if (Array.isArray(this.s.frames[this.cursor])) {
+            const max = Math.floor(this.s.frames[this.cursor].length - 1);
             const min = Math.ceil(0);
-            const selectedIdx = Math.floor(Math.random() * (max - min + 1)) + min;
-            return this.s.frames[ this.cursor ][selectedIdx]
+            const selectedIdx =
+              Math.floor(Math.random() * (max - min + 1)) + min;
+            return this.s.frames[this.cursor][selectedIdx];
           }
-          return this.s.frames[ this.cursor ]
+          return this.s.frames[this.cursor];
 
-        }else{
-          clearInterval(this.intervalid)
-          return this.s.frames[ this.cursor-1 ]
+        } else {
+          this.flow = 'ready'
+          this.cursor--;
+          clearInterval(this.intervalid);
+          return this.s.frames[this.cursor ];
+         
         }
       }
-      return null
-    }
+      return null;
+    },
   },
   components: {
     ButtonAccentBig,
@@ -99,7 +143,7 @@ export default {
     IconStick,
     IconChangeDirection,
     IconStop,
-    IconBack
+    IconBack,
   },
 };
 </script>
@@ -107,140 +151,240 @@ export default {
 <style  lang="scss">
 @import "../styles/media";
 
-
-.countdown{
-  position:absolute;
-  width:100vw;
+.countdown {
+  position: absolute;
+  width: 100vw;
   height: 100vh;
-  background-color: rgba(0,0,0, .8);
-  color: white; 
-  text-align:center;
-  line-height: 100 vh;
-  font-size:100vh;
-  font-weight: bolder;
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+  text-align: center;
+  line-height: 100vh;
+  font-size: 100vh;
+  font-weight: 400;
   font-family: var(--font-num);
 }
 
-.player{
+.display {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-color: transparent;
   display: flex;
-  flex-direction: row;
-  cursor:pointer;
-  position:relative;
+  flex-direction: column;
+
+  .display_header {
+    display:flex;
+    height: 50px;
+    gap:5px;
+
+    .button{
+      display: flex;
+      justify-content: center;
+      align-items:center;
+      font-size: 20px;
+      color: rgba(0,0,0, 0.5);
+      font-weight: 700;
+      border: 1px dotted rgba(255,255,255, .7);
+    }
+
+    .header_back-button {
+      height:100%;
+      width: 7%;
+    }
+    .header_info {
+      flex:1;
+      display: flex;;
+       justify-content: space-between;
+      align-items:center;
+      padding: 0 20px;
+
+    }
+    .header_view-button {
+     width: 7%;
+    }
+  }
+
+  .display_control {
+
+    display:flex;
+    flex:1;
+
+    .control_left-area {
+      flex:1;
+      border-right: 1px dotted rgba(255,255,255, .7);
+    }
+    .control_center-area {
+      width:60%;
+    }
+    .control_right-area {
+
+      border-left: 1px dotted rgba(255,255,255, .7);
+      flex:1;
+    }
+  }
 }
 
-.action{
+.player {
+  display: flex;
+  flex-direction: row;
+  cursor: pointer;
+  position: relative;
+}
+
+.action {
   height: 100vh;
   color: white;
   flex: 1;
 
-  &.black{
-    background-color:  black;
+  &.black {
+    background-color: black;
     color: white;
   }
-  
-  &.yellow{
+
+  &.yellow {
     background-color: var(--c-yellow);
     color: black;
   }
 
-  &.blue{
+  &.blue {
     background-color: var(--c-blue);
+    color: white;
   }
 
-  &.green{
+  &.green {
     background-color: var(--c-green);
     color: black;
   }
 
-  &.red{
+  &.red {
     background-color: var(--c-red);
-    color: black;
+    color: white;
   }
 }
 
-.counter{
+.counter {
   font-family: var(--font-num);
   height: 100vh;
-  background-color: rgb(0, 0, 0);
+  color: black;
+  background-color: white;
   font-size: 100vh;
   text-align: center;
-  font-weight: bolder;
+  font-weight: 400;
   line-height: 100vh;
   flex: 1;
-
-  &.yellow{
-    color: var(--c-yellow);
-  }
-
-  &.blue{
-    color: var(--c-blue);
-  }
-
-  &.green{
-    color: var(--c-green);
-  }
-
-  &.red{
-    color: var(--c-red);
-  }
 }
 
-.calc{
+.calc {
   height: 100vh;
   font-family: var(--font-num);
-  background-color: rgb(0, 0, 0);
+  background-color: black;
   text-align: center;
-  font-weight: bolder;
+  font-weight: 400;
   line-height: 100vh;
-  font-size:250px;
+  font-size: 250px;
+  letter-spacing: 10px;
   flex: 1;
 
-  &.yellow{
-    color: var(--c-yellow);
+  &.yellow {
+    background-color: var(--c-yellow);
+    color: black;
   }
 
-  &.blue{
-    color: var(--c-blue);
+  &.blue {
+    background-color: var(--c-blue);
+    color: white;
   }
 
-  &.green{
-    color: var(--c-green);
+  &.green {
+    background-color: var(--c-green);
+    color: black;
   }
 
-  &.red{
-    color: var(--c-red);
+  &.red {
+    background-color: var(--c-red);
+    color: white;
+
   }
 }
 </style>
 
 <template>
+  <div class="wrapper player">
+    <div v-if="countdown > 0 && useCountdown" class="countdown">{{ countdown }}</div>
 
-  <div class="wrapper player" v-on:dblclick="back" v-on:click="init">
+    <div class="display">
+      <div class="display_header">
+        <div class="header_back-button button" @click="back">B</div>
+        <div v-if="s" class="header_info">
+          <div>{{this.flow}}</div>
+          <div>{{cursor+1}} of {{s.frames.length}}</div>
+          <div>dur. {{currentFrame.duration}}</div>
+            
+        </div>
+        <div class="header_view-button button"  @click="fullScreen">F</div>
+        <div class="header_view-button button"  @click="viewer">V</div>
+      </div>
+      <div class="display_control">
+        <div class="control_left-area" v-on:click="staticPrev"></div>
+        <div class="control_center-area" v-on:dblclick="init" v-on:click="player"></div>
+        <div class="control_right-area" v-on:click="staticNext"></div>
+      </div>
+    </div>
 
-    <div v-if="countdown  > 0 " class="countdown">{{countdown}}</div>
-
-    <template  v-if="currentFrame && currentFrame.type==='action'">
-      <div class="action" 
+    <template v-if="currentFrame && currentFrame.type === 'action'">
+      <div
+        class="action"
         v-bind:key="`${div.color}_${index}`"
         v-for="(div, index) in currentFrame.divs"
-        :class="div.color">
-          <IconChangeDirection  v-if="currentFrame && div.icon==='change'" w="100%" h="100%"  />
-          <IconShoot  v-if="currentFrame && div.icon==='shot'" w="100%" h="100%"  />
-          <IconRun  v-if="currentFrame && div.icon==='run'" w="100%" h="100%"  />
-          <IconPass  v-if="currentFrame && div.icon==='pass'" w="100%" h="100%"  />
-          <IconStick  v-if="currentFrame && div.icon==='stick'" w="100%" h="100%"  />
-          <IconStop  v-if="currentFrame && div.icon==='control'" w="100%" h="100%"  />
-          <IconBack  v-if="currentFrame && div.icon==='back'" w="100%" h="100%"  />
+        :class="div.color"
+      >
+        <IconChangeDirection
+          v-if="currentFrame && div.icon === 'change'"
+          w="100%"
+          h="100%"
+        />
+        <IconShoot
+          v-if="currentFrame && div.icon === 'shot'"
+          w="100%"
+          h="100%"
+        />
+        <IconRun v-if="currentFrame && div.icon === 'run'" w="100%" h="100%" />
+        <IconPass
+          v-if="currentFrame && div.icon === 'pass'"
+          w="100%"
+          h="100%"
+        />
+        <IconStick
+          v-if="currentFrame && div.icon === 'stick'"
+          w="100%"
+          h="100%"
+        />
+        <IconStop
+          v-if="currentFrame && div.icon === 'control'"
+          w="100%"
+          h="100%"
+        />
+        <IconBack
+          v-if="currentFrame && div.icon === 'back'"
+          w="100%"
+          h="100%"
+        />
       </div>
     </template>
-  
-    <div class="counter" :class="currentFrame.divs[0].color" v-if="currentFrame && currentFrame.type==='counter'">
-      {{currentFrame.divs[0].value}}
+
+    <div
+      class="counter"
+      v-if="currentFrame && currentFrame.type === 'counter'"
+    >
+      {{ currentFrame.value }}
     </div>
 
-    <div class="calc" :class="currentFrame.divs[0].color" v-if="currentFrame && currentFrame.type==='calc'">
-      {{currentFrame.divs[0].value}}
+    <div
+      class="calc"
+      :class="currentFrame.color"
+      v-if="currentFrame && currentFrame.type === 'calc'"
+    >
+      {{ currentFrame.value }}
     </div>
-
   </div>
 </template>
--- client/src/views/Viewer.vue
+++ client/src/views/Viewer.vue
@@ -1,12 +1,18 @@
 <script>
-import ButtonAccentBig from "../components/buttons/button-accent-big.vue";
+import IconChangeDirection from "../components/action-icons/icon-change-direction.vue";
+import IconPass from "../components/action-icons/icon-pass.vue";
+import IconRun from "../components/action-icons/icon-run.vue";
+import IconShoot from "../components/action-icons/icon-shoot.vue";
+import IconStick from "../components/action-icons/icon-stick-handling.vue";
+import IconStop from "../components/action-icons/icon-stop-control.vue";
+import IconBack from "../components/action-icons/icon-back.vue";
 
 export default {
   name: "Viewer",
   inject: ["$services"],
   data: () => ({
     t: {},
-   
+    s: undefined
   }),
   methods: {
 
@@ -17,15 +23,20 @@ export default {
   },
 
   mounted() {
-    const _ = this;
-
-   
+    const seqId = this.$route.query.id;
+     this.s = this.$services.toolService.getSequenceById(seqId);
   },
   computed: {
    
   },
   components: {
-    ButtonAccentBig,
+        IconPass,
+    IconRun,
+    IconShoot,
+    IconStick,
+    IconChangeDirection,
+    IconStop,
+    IconBack,
   },
 };
 </script>
@@ -33,13 +44,269 @@ export default {
 <style  lang="scss">
 @import "../styles/media";
 
+.viewer{
+  position: relative;
+  background-color:#bfbfbf;
+}
+
+.carrousel{
+  width: 90%;
+  height: 100vh;
+  overflow-x: scroll;
+  display: flex;
+  gap: 10px;
+  top:10px;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  position:absolute;
+  align-items: center;
+}
+
+.diapo{
+  min-width: 190px;
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+
+  &.asColum{
+    gap: 10px;
+    flex-direction: column;
+    height:auto;
+  }
+}
 
 
+.mini-action {
+  min-height: 100px;
+  color: white;
+  flex: 1;
+
+  &.black {
+    background-color: black;
+    color: white;
+  }
+
+  &.yellow {
+    background-color: var(--c-yellow);
+    color: black;
+  }
+
+  &.blue {
+    background-color: var(--c-blue);
+    color: white;
+  }
+
+  &.green {
+    background-color: var(--c-green);
+    color: black;
+  }
+
+  &.red {
+    background-color: var(--c-red);
+    color: white;
+  }
+}
+
+.mini-counter {
+  font-family: var(--font-num);
+  min-height: 100px;
+  color: black;
+  background-color: white;
+  font-size: 50px;
+  text-align: center;
+  font-weight: 400;
+  line-height: 100px;
+  flex: 1;
+}
+
+.mini-calc {
+  min-height: 100px;
+  font-family: var(--font-num);
+  background-color: black;
+  text-align: center;
+  font-weight: 400;
+  line-height: 100px;
+  font-size: 50px;
+  letter-spacing: 10px;
+  flex: 1;
+
+  &.yellow {
+    background-color: var(--c-yellow);
+    color: black;
+  }
+
+  &.blue {
+    background-color: var(--c-blue);
+    color: white;
+  }
+
+  &.green {
+    background-color: var(--c-green);
+    color: black;
+  }
+
+  &.red {
+    background-color: var(--c-red);
+    color: white;
+
+  }
+}
 
 </style>
 
 <template>
   <div class="viewer wrapper">
-   
+    <template v-if="s" >
+      <div class="carrousel" >
+
+        <div class="diapo" :class="{asColum: Array.isArray(currentFrame)}" v-bind:key="`${currentFrame.type}_${index}`"  
+        v-for="(currentFrame, index) in s.frames">
+
+
+          <template v-if="Array.isArray(currentFrame)">
+            <template  v-for="(frame, index) in currentFrame">
+            
+<!-- start -->
+<template v-if="frame && frame.type === 'action'">
+              <div
+                class="mini-action"
+                v-bind:key="`${div.color}_${index}`"
+                v-for="(div, index) in frame.divs"
+                :class="div.color"
+              >
+              <IconChangeDirection
+                v-if="frame && div.icon === 'change'"
+                w="100%"
+                h="100%"
+              />
+              <IconShoot
+                v-if="frame && div.icon === 'shot'"
+                w="100%"
+                h="100%"
+              />
+              <IconRun v-if="frame && div.icon === 'run'" w="100%" h="100%" />
+              <IconPass
+                v-if="frame && div.icon === 'pass'"
+                w="100%"
+                h="100%"
+              />
+              <IconStick
+                v-if="frame && div.icon === 'stick'"
+                w="100%"
+                h="100%"
+              />
+              <IconStop
+                v-if="frame && div.icon === 'control'"
+                w="100%"
+                h="100%"
+              />
+              <IconBack
+                v-if="frame && div.icon === 'back'"
+                w="100%"
+                h="100%"
+              />
+            </div>
+          </template>
+
+          <div
+            class="mini-counter"
+            v-bind:key="`${frame.color}_${index}`"
+            v-if="frame && frame.type === 'counter'"
+          >
+            {{ frame.value }}
+          </div>
+
+          <div
+            class="mini-calc"
+            :class="frame.color"
+            v-bind:key="`${frame.color}_${index}`"
+            v-if="frame && frame.type === 'calc'"
+          >
+            {{ frame.value }}
+          </div>
+
+
+
+
+            <!--  end -->
+            </template>
+          </template>
+
+
+          <template v-if="!Array.isArray(currentFrame)">
+
+
+            <template v-if="currentFrame && currentFrame.type === 'action'">
+              <div
+                class="mini-action"
+                v-bind:key="`${div.color}_${index}`"
+                v-for="(div, index) in currentFrame.divs"
+                :class="div.color"
+              >
+              <IconChangeDirection
+                v-if="currentFrame && div.icon === 'change'"
+                w="100%"
+                h="100%"
+              />
+              <IconShoot
+                v-if="currentFrame && div.icon === 'shot'"
+                w="100%"
+                h="100%"
+              />
+              <IconRun v-if="currentFrame && div.icon === 'run'" w="100%" h="100%" />
+              <IconPass
+                v-if="currentFrame && div.icon === 'pass'"
+                w="100%"
+                h="100%"
+              />
+              <IconStick
+                v-if="currentFrame && div.icon === 'stick'"
+                w="100%"
+                h="100%"
+              />
+              <IconStop
+                v-if="currentFrame && div.icon === 'control'"
+                w="100%"
+                h="100%"
+              />
+              <IconBack
+                v-if="currentFrame && div.icon === 'back'"
+                w="100%"
+                h="100%"
+              />
+            </div>
+          </template>
+
+          <div
+            class="mini-counter"
+            v-if="currentFrame && currentFrame.type === 'counter'"
+          >
+            {{ currentFrame.value }}
+          </div>
+
+          <div
+            class="mini-calc"
+            :class="currentFrame.color"
+            v-if="currentFrame && currentFrame.type === 'calc'"
+          >
+            {{ currentFrame.value }}
+          </div>
+
+          
+          </template>
+
+
+
+
+        
+          
+
+
+
+        </div>
+      </div>
+    </template>
   </div>
 </template>
